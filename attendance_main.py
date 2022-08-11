a9b1c2fc7f7c0829d9e38676be1706e4ec565e63
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-08-11 23:23:18 +0800

    revamped approach to allow students to enter their attendance, weapon draw in/out and pellet count

--- attendance_main.py
+++ attendance_main.py
@@ -1,13 +1,10 @@
-from openpyxl import Workbook
-
-class Attendance_Entry:
-    def __init__(self, name, level, date):
+class New_Entry:
+    def __init__(self, name, date, timein, timeout, weaponid, pelletcount):
         self.name = name
-        self.level = level
         self.date = date
-    
-    def write_to_excel(self): #writes yes in the correct cell according to name and date
-        pass
-Luth = Attendance_Entry('Luth', '3C', '26/7/2022')
-Luth.write_to_excel()
+        self.timein = timein
+        self.timeout = timeout
+        self.weaponid = weaponid
+        self.pelletcount = pelletcount
 
+class 
