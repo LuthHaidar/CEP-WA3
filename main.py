850f3e9199bb1588065283c841241c0caa3f22b7
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-08-28 00:08:50 +0800

    Fixed some stuff
    Added some comments
    
    Removed a problematic line that was for a future feature

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from time import sleep
 from settings import *
 
 
-pd.set_option("display.colheader_justify", "center")
-pd.df.style.set_table_styles([{'selector' : '', 'props' : [('border', '2px solid green')]}])
+pd.set_option("display.colheader_justify", "center") # dataframe formatting
 
-class New_Entry:
-    def req(self):  # Requests user for info
-        # variables: name, date, timein, timeout, weaponid, pelletcount
+
+class New_Entry: # creates a new entry
+    def req(self): # Requesting user input
         name = input("Please enter your name: ")
         date = input("Please enter the date: ")
         timein = input("Please enter the time you entered: ")
@@ -20,20 +19,23 @@ class New_Entry:
         pelletcount = input("Please enter the pellet count: ")
         list1 = [name, date, timein, timeout, weaponid, pelletcount]
         return list1
+    
+    def read(self):
+        df = pd.DataFrame(self.list1, columns = ["Name", "Date", "Time In", "Time Out", "Weapon ID", "Pellet Count"], index = [''])
 
 
-class Todays_entries:
-    def __init__(self, list1):
+class Todays_entries: # holds/manages current session's entries
+    def __init__(self, list1): # requires compilation of New_Entries from compile_entries
         self.list1 = list1
 
-    def savedf(self):
+    def savedf(self): # saves the dataframe to xlsx file
         wb = openpyxl.load_workbook("E:/CEP-WA3/attendance.xlsx")
         ws = wb.active
         for i in range(len(self.list1)):
             ws.append(self.list1[i])
         wb.save("E:/CEP-WA3/attendance.xlsx")
 
-    def read(self):
+    def read(self): # reads the current session's entries
         df = pd.DataFrame(
             self.list1,
             columns=[
@@ -46,15 +48,15 @@ class Todays_entries:
             ],
             index=[""] * len(self.list1),
         )
-        print(df)
+        return df
 
 
-class All_entries:
-    def read(self):
+class All_entries: # facilitates interaction with the main database
+    def read(self): # reads the entire database
         df = pd.read_excel("E:/CEP-WA3/attendance.xlsx")
         print(df)
 
-    def search(self, column, value):
+    def search(self, column, value): # searches the database for a specific value in a specific column
         df = pd.read_excel("E:/CEP-WA3/attendance.xlsx")
         df = df.loc[df[column] == value]
         print(df)
@@ -64,6 +66,7 @@ def compile_entries():  # compiles entries into a list
     list1 = []
     entry1 = New_Entry()
     yes = True
+    
     while yes == True:
         list1.append(entry1.req())
         choice = input("Do you want to add another entry? (y/n): ")
@@ -73,16 +76,17 @@ def compile_entries():  # compiles entries into a list
             yes = False
         else:
             print("Please enter a valid choice.")
+    
     return list1
 
 
-def start():
+def start(): # to be called later in order to start the program
     print("Welcome to the Shooting Club Attendance Tracker.")
     sleep(0.5)
     main()
 
 
-def main():
+def main(): # main process
     while True:
         print("You can do the following things:")
         sleep(0.5)
@@ -91,22 +95,26 @@ def main():
         print("3. Search entries")
         print("4. Exit")
         sleep(0.5)
+        
         choice = input("Please enter your choice: ")
         os.system("cls")
-        if choice == "1":
+        
+        if choice == "1": # add a new entry
             list1 = compile_entries()
             entry1 = Todays_entries(list1)
             entry1.savedf()
             input()
             os.system("cls")
-        elif choice == "2":
+        
+        elif choice == "2": # view current session's entries
             try:
                 entry1.read()
             except:
                 print("There are no entries for today.")
             input()
             os.system("cls")
-        elif choice == "3":
+        
+        elif choice == "3": # search entries
             print('Column 1 is "Name", Column 2 is "Date", Column 3 is "Time In",\nColumn 4 is "Time Out", Column 5 is "Weapon ID"and Column 6 is "Pellet Count."')
             sleep(0.5)
             entry2 = All_entries()
@@ -117,10 +125,12 @@ def main():
             entry2.search(column, value)
             input()
             os.system("cls")
-        elif choice == "4":
+        
+        elif choice == "4": # exits the program
             if input("Are you sure you want to exit? (y/n): ") == "y":
                 exit()
-        else:
+        
+        else: # failsafe
             print("Please enter a valid choice.")
             os.system("cls")
 
